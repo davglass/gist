fc2f0fb483483120db40b1dd0fa1b32ea3c6e352
Jeremy Banks <jeremy@jeremybanks.ca>
2009-01-25 18:15:11 -0500

    GistUser class created, initialization method written.
    Authentication information can be manually provided when creating a new
    instance, otherwise the program will attempt to load it from the git
    config. If that fails as well, an unauthenticated user wil be created.

--- gist.py
+++ gist.py
@@ -1,15 +1,62 @@
-#!/usr/bin/env python
-# encoding: utf-8
+#!/usr/bin/env python3.0
 import sys
-import urllib
+import urllib.request
 import optparse
-import subprocess	
+import subprocess
 
-GIST_URL = "http://gist.github.com/{0}.txt"
+URL_GIST = "http://gist.github.com/"
+URL_GIST_TXT = URL_GIST + "{id}.txt"
+URL_GIST_POST = URL_GIST + "gists"
+
+def loadAuthentication():
+	"""
+		Loads GitHub/Gist authentication information from the git config,
+		
+		The authentication information is returned as a tuple (user, token).
+		If authentication information is unavailable or incomplete, None
+		is returned.
+	"""
+	
+	user = subprocess.Popen(["git", "config", "github.user"], stdout=subprocess.PIPE).communicate()[0].decode("UTF-8").strip()
+	token = subprocess.Popen(["git", "config", "github.token"], stdout=subprocess.PIPE).communicate()[0].decode("UTF-8").strip()
+	
+	if user and token:
+		return(user, token)
+	else:
+		return(None)
+
+class GistUser(object):
+	"""
+		Represents a Gist user, including authentication information.
+	"""
+	
+	def __init__(self, authentication=None):
+		"""
+			Iniitalizes a GistUser()
+			
+			Authentication information may be provided in the form of a tuple
+			(user, token), otherwise it will be attempted to be loaded from
+			the git config variables github.user and github.token.
+		"""
+		
+		if authentication:
+			if len(authentication) == 2:
+				self.authentication = tuple(authentication)
+			else:
+				raise ValueError("Authenication tuple must be of length 2.")
+		else:
+			self.authentication = loadAuthentication()
+	
+	def __repr__(self):
+		if self.authentication:
+			return("GistUser({0})".format(self.authentication))
+		else:
+			return("GistUser(authentication=None)")
 
 def main(*args):
 	import gist
 	
-	print(GIST_URL.format("HAM"))
+	user = gist.GistUser()
+	print(user)
 
 if __name__ == "__main__": sys.exit(main(*sys.argv[1:]))
