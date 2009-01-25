5c45b18cb3e3f102fc11214ff547d8f672d88d30
Dav Glass <davglass@gmail.com>
2009-01-25 15:16:20 -0800

    Added copy to clipboard

--- gist.py
+++ gist.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 from optparse import OptionParser
-import os, sys, platform, string, pprint
+import os, sys, string, pprint
 from cStringIO import StringIO
-import urllib2, urllib
+import urllib2, urllib, subprocess
 
-
-optparser = OptionParser("usage: %prog [-p] file1 file2 file3\n\n
-Pass files to me and I'll post them to http://gist.github.com")
+optparser = OptionParser("usage: %prog [-p] file1 file2 file3\n\nPass files to me and I'll post them to http://gist.github.com")
 optparser.set_defaults(gistread="")
 optparser.add_option( "-p", "--private",
         action="store_true", dest="private",
@@ -21,9 +19,19 @@ optparser.add_option( "-r", "--read",
 
 
 def copy(content):
-    system = platform.system()
-    if system == 'Darwin':
-        os.popen('pbcopy', 'r').write(content)
+    system = sys.platform
+    if system == 'darwin':
+        p = subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE)
+        p.stdin.write(content)
+        p.stdin.close()
+        retcode = p.wait()
+    elif system == 'linux':
+        p = subprocess.Popen(['xclip'], stdin=subprocess.PIPE)
+        p.stdin.write(content)
+        p.stdin.close()
+        retcode = p.wait()
+        
+        
 
 def write(filenames):
     out = {}
@@ -57,7 +65,7 @@ def write(filenames):
     response = urllib2.urlopen(req)
 
     url = response.geturl()
-    #copy(url)
+    copy(url)
     print url
 
 
